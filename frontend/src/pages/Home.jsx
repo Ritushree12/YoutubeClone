8494e3e1f08a57a5041308be0cb8337a73b90e81
Ritushree L <56196065+Ritushree12@users.noreply.github.com>
2026-01-08 20:27:43 +0530

    Made changes to the home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,19 +1,44 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
 import VideoCard from "../components/VideoCard";
+import Filters from "../components/Filters";
 
 const Home = () => {
   const [videos, setVideos] = useState([]);
+  const [search, setSearch] = useState("");
+  const [category, setCategory] = useState("All");
 
   useEffect(() => {
-    api.get("/videos").then((res) => setVideos(res.data));
-  }, []);
+    // Fetch videos based on search and category
+    api
+      .get(
+        `/videos?search=${search}&category=${
+          category === "All" ? "" : category
+        }`
+      )
+      .then((res) => setVideos(res.data))
+      .catch((err) => console.error(err));
+  }, [search, category]);
 
   return (
     <div>
-      {videos.map((v) => (
-        <VideoCard key={v._id} video={v} />
-      ))}
+      {/* Search Input */}
+      <input
+        type="text"
+        placeholder="Search videos..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      {/* Category Filters */}
+      <Filters setCategory={setCategory} />
+
+      {/* Video Grid */}
+      <div className="video-grid">
+        {videos.map((v) => (
+          <VideoCard key={v._id} video={v} />
+        ))}
+      </div>
     </div>
   );
 };
